a5b712cd9890ca746015ee79c942f5c7bfc7dbd9
Will Yager <will.yager@gmail.com>
2017-08-12 18:57:02 -0600

    fix formatter

--- format.sh
+++ format.sh
@@ -1,7 +1,7 @@
 for FILE in *.lhs; do
     FILE="${FILE%%.*}"
     pandoc $FILE.lhs --standalone --self-contained -o $FILE.html
-    replace="<title>Building a CPU (Part 1) :: Will Yager</title> <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'> <link href='/post.css' rel='stylesheet' type=\"text/css\">"
+    replace="<title>Building a CPU :: Will Yager</title> <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'> <link href='/post.css' rel='stylesheet' type=\"text/css\">"
     replaceEscaped=$(sed 's/[&/\]/\\&/g' <<<"$replace")
     sed -i -e "s/<title><\/title>/$replaceEscaped/g" $FILE.html
 done
